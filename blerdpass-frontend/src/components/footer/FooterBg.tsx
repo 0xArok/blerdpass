cf34127d8cbddce365164f2bf6868bf9f5dcf28d
nativityinblack.eth <davanoraindika@protonmail.com>
2022-07-08 07:40:29 +0800

    blerdpass

--- blerdpass-frontend/src/components/footer/FooterBg.tsx
+++ blerdpass-frontend/src/components/footer/FooterBg.tsx
@@ -1,8 +1,8 @@
 import {NextPage} from "next";
 import {Stack, Typography, Box} from "@mui/material";
 import Image from 'next/image'
-import twitter from '../../../public/twitter.png';
-import discord from '../../../public/discord.png';
+import twitter from '../../../public/twitterlogo.png';
+import discord from '../../../public/discordlogobp.png';
 import telegram from '../../../public/telegram.png';
 import odyssey from '../../../public/logo_odyssey.png'
 import eth from '../../../public/eth.png';
@@ -11,25 +11,31 @@ import {Custom} from "../../styles/Theme";
 function Footer() {
 
     return (
-        <Stack direction="column" justifyContent="center" height="100%"
-               alignItems="center" paddingY={3} spacing={3} sx={{background: '#0a070a'}} width={"100vw"} zIndex={10}>
+        <Stack direction="row"
+               justifyContent="space-between"
+               height="100%"
+               alignItems="center" paddingY={3}
+               spacing={3}
+               sx={{background: '#0a070a'}}
+               width={"100vw"}
+               zIndex={10}
+               paddingLeft={5} >
+            <Typography variant={"h2"} color={"#fff"}>
+                Terms and Condition
+            </Typography>
 
-
-            <Stack direction={"row"} alignItems="center" justifyContent="center" spacing={3} paddingY={5}>
+            <Stack direction={"row"} alignItems="center" justifyContent="center" spacing={1} paddingY={3}>
                 <a href={"https://twitter.com/kumaversenft"} target="_blank" rel="noreferrer">
-                    <Image src={twitter} alt="twitter" width={72} height={72}/>
+                    <Image src={twitter} alt="twitter" width={80} height={52}/>
                 </a>
                 <a href={"https://discord.gg/kumaverse"} target="_blank" rel="noreferrer">
-                    <Image src={discord} alt="discord" width={72} height={72}/>
+                    <Image src={discord} alt="discord" width={90} height={62}/>
                 </a>
 
             </Stack>
 
 
-            <Typography textAlign={"center"} variant={"caption"} color={"white"}>
 
-                Copyright 2022 SAINT ROCK MEDIA<br/>
-            </Typography>
 
         </Stack>
     )
