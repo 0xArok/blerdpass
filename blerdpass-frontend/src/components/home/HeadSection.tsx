dd26cda046de2f9fa6de738af50c14cb937860e8
nativityinblack.eth <davanoraindika@protonmail.com>
2022-07-09 05:53:03 +0800

    100vh

--- blerdpass-frontend/src/components/home/HeadSection.tsx
+++ blerdpass-frontend/src/components/home/HeadSection.tsx
@@ -18,7 +18,7 @@ function HeadSection() {
   const matches = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
-    <Stack height={matches ? "90vh" : "80vh"} justifyContent={"center"} alignItems={"center"} justifyItems={"center"} sx={{backgroundColor: "#001051"}}>
+    <Stack height={matches ? "100vh" : "80vh"} justifyContent={"center"} alignItems={"center"} justifyItems={"center"} sx={{backgroundColor: "#001051"}}>
 
       {connected ?
           <Stack paddingBottom={12} marginTop={12} paddingTop={6}>
